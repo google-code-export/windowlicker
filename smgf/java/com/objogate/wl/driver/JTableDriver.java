91f4a4f53dc35094d52bcc29c1cc608c5f3282ab
smgfreeman <smgfreeman@ac37430e-0f47-0410-9073-f3f9481b7c33>
2008-04-25 08:18:27 +0000

    Branch to try new things out on

---
+++ smgf/java/com/objogate/wl/driver/JTableDriver.java
@@ -0,0 +1,421 @@
+package com.objogate.wl.driver;
+
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import com.objogate.exception.Defect;
+import com.objogate.wl.*;
+import com.objogate.wl.gesture.GesturePerformer;
+import com.objogate.wl.gesture.Gestures;
+import static com.objogate.wl.gesture.Gestures.*;
+import com.objogate.wl.gesture.Tracker;
+
+public class JTableDriver extends ComponentDriver<JTable> {
+
+    public JTableDriver(ComponentDriver<? extends Container> containerDriver, Matcher<? super JTable>... matchers) {
+        super(containerDriver, JTable.class, matchers);
+    }
+
+    public JTableDriver(GesturePerformer gesturePerformer, JTable component) {
+        super(gesturePerformer, component);
+    }
+
+    public JTableDriver(GesturePerformer gesturePerformer, JTable component, Prober prober) {
+        super(gesturePerformer, component, prober);
+    }
+
+    public JTableDriver(GesturePerformer gesturePerformer, ComponentSelector<JTable> componentSelector) {
+        super(gesturePerformer, componentSelector);
+    }
+
+    public JTableDriver(GesturePerformer gesturePerformer, ComponentSelector<JTable> componentSelector, Prober prober) {
+        super(gesturePerformer, componentSelector, prober);
+    }
+
+    public JTableDriver(ComponentDriver<? extends Component> parentOrOwner, ComponentSelector<JTable> componentSelector) {
+        super(parentOrOwner, componentSelector);
+    }
+
+    public JTableDriver(ComponentDriver<? extends Component> parentOrOwner, Class<JTable> componentType, Matcher<? super JTable>... matchers) {
+        super(parentOrOwner, componentType, matchers);
+    }
+
+    public static boolean arrayContains(int[] stuff, int item) {
+        for (int selectedRow : stuff) {
+            if (item == selectedRow)
+                return true;
+        }
+        return false;
+    }
+
+    public void selectCells(Cell... cells) {
+        scrollCellToVisible(cells[0]);
+        
+        Gesture[] gestures = new Gesture[cells.length];
+        for (int i = 0; i < cells.length; i++) {
+           gestures[i] = sequence(
+                   moveMouseTo(pointIn(cells[i])),
+                   leftClickMouse()
+           );
+        }
+
+        performGesture(whileHoldingMultiSelect(sequence(gestures)));
+    }
+
+    public void selectCell(final int row, final int col) {
+        selectCells(cell(row, col));
+    }
+
+    public void selectCell(final Matcher<? extends JComponent> matcher) {
+        final Cell cell = hasCell(matcher);
+
+        if (cell == null)
+            throw new Defect("Cannot find cell");
+
+        selectCells(cell);
+    }
+
+    public void dragMouseOver(Cell start, Cell end) {
+        scrollCellToVisible(start);
+
+        performGesture(
+                moveMouseTo(pointIn(start)),
+                whileHoldingMouseButton(Gestures.BUTTON1,
+                        moveMouseTo(pointIn(end)))
+        );
+    }
+
+    private Tracker pointIn(Cell start) {
+        return offset(relativeMidpointOfColumn(start.col), rowOffset(start.row));
+    }
+
+    public Cell hasCell(final Matcher<? extends JComponent> matcher) {
+        WithCellMatcher withCellMatching = new WithCellMatcher(matcher);
+        
+        is(withCellMatching);
+
+        return withCellMatching.foundCell;
+    }
+
+    public Component editCell(int row, int col) {
+        mouseOverCell(row, col);
+        performGesture(Gestures.doubleClickMouse());
+
+        JTableCellManipulation manipulation = new JTableCellManipulation(row, col);
+        perform("finding cell editor", manipulation);
+
+        return manipulation.getEditorComponent();
+    }
+
+    public void mouseOverCell(Cell cell) {
+        mouseOverCell(cell.row, cell.col);
+    }
+
+    public void mouseOverCell(int row, int col) {
+        scrollCellToVisible(row, col);
+
+        int y = rowOffset(row);
+        int x = relativeMidpointOfColumn(col);
+
+        moveMouseToOffset(x, y);
+    }
+
+    private int rowOffset(int row) {
+        int rowHeight = rowHeight();
+        return (rowHeight * row) + (rowHeight / 2);
+    }
+
+    private int relativeMidpointOfColumn(final int col) {
+        ColumnManipulation manipulation = new ColumnManipulation(col);
+        perform("column mid point", manipulation);
+        return manipulation.getMidPoint();
+    }
+
+    private int rowHeight() {
+        JTableRowHeightManipulation tableManipulation = new JTableRowHeightManipulation();
+        perform("row height", tableManipulation);
+        return tableManipulation.getRowHeight();
+    }
+
+    public void hasSelectedCells(final Cell... cells) {
+        is(new SelectedCellsMatcher(cells));
+    }
+
+    public void scrollCellToVisible(Cell cell) {
+        scrollCellToVisible(cell.row, cell.col);
+    }
+
+    //todo (nick): this should be a gesture
+    public void scrollCellToVisible(final int row, final int col) {
+        perform("table scrolling", new ComponentManipulation<JTable>() {
+            public void manipulate(JTable table) {
+                table.scrollRectToVisible(table.getCellRect(row, col, true));
+            }
+        });
+    }
+
+    public void cellHasColour(int row, Object columnIdentifier, Matcher<Color> foregroundColor, Matcher<Color> backgroundColor) {
+        cellHasBackgroundColor(row, columnIdentifier, backgroundColor);
+        cellHasForegroundColor(row, columnIdentifier, foregroundColor);
+    }
+
+    public void cellHasColour(int row, int col, Matcher<Color> foregroundColor, Matcher<Color> backgroundColor) {
+        cellHasBackgroundColor(row, col, backgroundColor);
+        cellHasForegroundColor(row, col, foregroundColor);
+    }
+
+    public void cellHasBackgroundColor(final int row, final Object columnIdentifier, Matcher<Color> backgroundColor) {
+      cellHasBackgroundColor(cell(row, columnIdentifier), backgroundColor);
+    }
+
+    public void cellHasBackgroundColor(final int row, final int col, Matcher<Color> backgroundColor) {
+      cellHasBackgroundColor(cell(row, col), backgroundColor);
+    }
+    
+    public void cellHasBackgroundColor(final AbstractCell cell, Matcher<Color> backgroundColor) {
+      has(cellQuery(cell, backgroundColor()), backgroundColor);
+    }
+
+    public void cellHasForegroundColor(final int row, final Object columnIdentifier, Matcher<Color> foregroundColor) {
+      cellHasForegroundColor(cell(row, columnIdentifier), foregroundColor);
+    }
+
+    public void cellHasForegroundColor(final int row, final int col, Matcher<Color> foregroundColor) {
+      cellHasForegroundColor(cell(row, col), foregroundColor);
+    }
+    
+    public void cellHasForegroundColor(final AbstractCell cell, Matcher<Color> foregroundColor) {
+        has(cellQuery(cell, foregroundColor()), foregroundColor);
+    }
+    
+    public void cellRenderedWithText(final int row, final Object columnIdentifier, Matcher<String> expectedText) {
+        cellRenderedWithText(cell(row, columnIdentifier), expectedText);
+    }
+
+    public void cellRenderedWithText(final int row, final int col, Matcher<String> expectedText) {
+        cellRenderedWithText(cell(row, col), expectedText);
+    }
+
+    public void cellRenderedWithText(final AbstractCell cell, Matcher<String> expectedText) {
+      has(new CellTextQuery(cell), expectedText);
+    }
+
+    
+    public static <T> Query<JTable, T> cellQuery(AbstractCell cell, Query<Component, T> detail) {
+      return new CellQuery<T>(cell, detail);
+    }
+    
+    public static Query<Component, Color> foregroundColor() {
+      return new Query<Component, Color>() {
+        public Color query(Component component) { return component.getForeground(); }
+        public void describeTo(Description description) { description.appendText("foreground colour"); }
+      };
+    }
+
+    public static Query<Component, Color> backgroundColor() {
+      return new Query<Component, Color>() {
+        public Color query(Component component) { return component.getBackground(); }
+        public void describeTo(Description description) { description.appendText("background colour"); }
+      };
+    }
+
+    public static Query<Component, String> labelText(final AbstractCell location) {
+      return new Query<Component, String>() {
+        public String query(Component cell) {
+          if (cell instanceof JLabel) {
+            JLabel label = (JLabel) cell;
+            return label.getText();
+          } else {
+              throw new Defect("Rendered component in cell at " + location + " is not a JLabel but a " + cell.getClass().getName());
+          }
+        }
+
+        public void describeTo(Description description) {
+            description.appendText("text");
+        }
+      };
+    }
+    public static IndentifierCell cell(final int row, final Object columnIdentifier) {
+      return new IndentifierCell(row, columnIdentifier);
+    }
+
+    public static Cell cell(int row, int col) {
+      return new Cell(row, col);
+    }
+
+    public static abstract class AbstractCell {
+      public final int row;
+
+      public AbstractCell(int row) { this.row = row; }
+      public abstract Cell asCellIn(JTable table);
+    }
+    
+    
+    public static class Cell extends AbstractCell {
+        public final int col;
+
+        public Cell(int row, int col) {
+          super(row);
+          this.col = col;
+        }
+        public Object valueFrom(JTable table) {
+          return table.getValueAt(row, col);
+        }
+        @Override public Cell asCellIn(JTable unused) { return this; }
+
+        @Override public String toString() {
+            return "r" + row + " x " + "c" + col;
+        }
+    }
+
+    public static class IndentifierCell extends AbstractCell {
+      public final Object columnIdentifier;
+
+      public IndentifierCell(int row, Object columnIdentifier) {
+        super(row);
+        this.columnIdentifier = columnIdentifier;
+      }
+
+      @Override public Cell asCellIn(JTable table) {
+        return new Cell(row, viewIndex(table));
+      }
+      @Override public String toString() {
+          return "row " + row + " at " + columnIdentifier;
+      }
+      private int viewIndex(JTable table) {
+        int modelIndex = table.getColumn(columnIdentifier).getModelIndex();
+        return table.convertColumnIndexToView(modelIndex);
+      }
+  }
+
+    private class SelectedCellsMatcher extends TypeSafeMatcher<JTable> {
+        public Cell unselectedCell;
+        private final Cell[] cells;
+
+        public SelectedCellsMatcher(Cell... cells) {
+            this.cells = cells;
+        }
+
+        @Override
+        public boolean matchesSafely(JTable table) {
+            for (Cell cell : cells) {
+                if (!table.isCellSelected(cell.row, cell.col)) {
+                    this.unselectedCell = cell;
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public void describeTo(Description description) {
+            description.appendText("cell " + unselectedCell + " is not selected");
+        }
+    }
+    
+    private static final class WithCellMatcher extends TypeSafeMatcher<JTable> {
+      private final Matcher<? extends JComponent> matcher;
+      Cell foundCell;
+
+      WithCellMatcher(Matcher<? extends JComponent> matcher) {
+        this.matcher = matcher;
+      }
+
+      @Override public boolean matchesSafely(JTable table) {
+          int rowCount = table.getRowCount();
+          int columnCount = table.getColumnCount();
+
+          for (int row = 0; row < rowCount; row++) {
+              for (int col = 0; col < columnCount; col++) {
+                  Component renderedCell = JTableCellManipulation.render(table, row, col);
+                  if (matcher.matches(renderedCell)) {
+                      foundCell = new Cell(row, col);
+                      return true;
+                  }
+              }
+          }
+          return false;
+      }
+
+      public void describeTo(Description description) {
+          description.appendText("with cell ")
+                     .appendDescriptionOf(matcher);
+      }
+    }
+
+    private static class JTableRowHeightManipulation implements ComponentManipulation<JTable> {
+        private int rowHeight;
+
+        public void manipulate(JTable component) {
+            rowHeight = component.getRowHeight();
+        }
+
+        public int getRowHeight() {
+            return rowHeight;
+        }
+    }
+
+    private static class ColumnManipulation implements ComponentManipulation<JTable> {
+        private int midpoint;
+        private final int col;
+
+        public ColumnManipulation(int col) {
+            this.col = col;
+        }
+
+        public void manipulate(JTable component) {
+            midpoint = JTableHeaderDriver.ColumnManipulation.midpointOfColumn(col, component.getColumnModel());
+        }
+
+        public int getMidPoint() {
+            return midpoint;
+        }
+    }
+
+    private static class CellQuery<T> implements Query<JTable, T> {
+      private final AbstractCell location;
+      private final Query<Component, T> detail;
+
+      public CellQuery(AbstractCell location, Query<Component, T> detail) {
+          this.location = location;
+          this.detail = detail;
+      }
+
+      public T query(JTable table) {
+          return detail.query(JTableCellManipulation.render(table, location.asCellIn(table)));
+      }
+
+      public void describeTo(Description description) {
+          description.appendDescriptionOf(detail)
+                     .appendText(" in cell at " + location);
+      }
+    }
+
+    private static class CellTextQuery implements Query<JTable, String> {
+        private final AbstractCell location;
+
+        public CellTextQuery(AbstractCell location) {
+            this.location = location;
+        }
+
+        public String query(JTable table) {
+            Component cell = JTableCellManipulation.render(table, location.asCellIn(table));
+
+            if (cell instanceof JLabel) {
+                JLabel label = (JLabel) cell;
+                return label.getText();
+            } else {
+                throw new Defect("Rendered component in cell at " + location + " is not a JLabel but a " + cell.getClass().getName());
+            }
+        }
+
+        public void describeTo(Description description) {
+            description.appendText("text in cell at " + location);
+        }
+    }
+}
---
+++ smgf/java/com/objogate/wl/driver/JTextFieldDriver.java
@@ -0,0 +1,190 @@
+package com.objogate.wl.driver;
+
+import javax.swing.JTextField;
+import java.awt.Component;
+import java.awt.Insets;
+import java.awt.Point;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.junit.matchers.TypeSafeMatcher;
+import com.objogate.wl.ComponentManipulation;
+import com.objogate.wl.ComponentSelector;
+import com.objogate.wl.Prober;
+import com.objogate.wl.gesture.GesturePerformer;
+import com.objogate.wl.gesture.Gestures;
+import static com.objogate.wl.gesture.Gestures.*;
+import com.objogate.wl.gesture.Tracker;
+
+public class JTextFieldDriver extends JTextComponentDriver<JTextField> {
+    public JTextFieldDriver(GesturePerformer gesturePerformer, JTextField component) {
+        super(gesturePerformer, component);
+    }
+
+    public JTextFieldDriver(GesturePerformer gesturePerformer, JTextField component, Prober prober) {
+        super(gesturePerformer, component, prober);
+    }
+
+    public JTextFieldDriver(GesturePerformer gesturePerformer, ComponentSelector<JTextField> componentSelector) {
+        super(gesturePerformer, componentSelector);
+    }
+
+    public JTextFieldDriver(GesturePerformer gesturePerformer, ComponentSelector<JTextField> componentSelector, Prober prober) {
+        super(gesturePerformer, componentSelector, prober);
+    }
+
+    public JTextFieldDriver(ComponentDriver<? extends Component> parentOrOwner, ComponentSelector<JTextField> componentSelector) {
+        super(parentOrOwner, componentSelector);
+    }
+
+    public JTextFieldDriver(ComponentDriver<? extends Component> parentOrOwner, Class<JTextField> componentType, Matcher<? super JTextField>... matchers) {
+        super(parentOrOwner, componentType, matchers);
+    }
+
+    public void doubleClickText(TextOccurence textOccurence) {
+        TextSearchMatcher search = new TextSearchMatcher(textOccurence);
+        is(search);
+        int end = search.getEnd();
+
+        int scrollOffset = scrollPositionIntoView(end);
+
+        final int x = getTextWidth(0, end - 1) + getInsets().left - scrollOffset - 1;
+        final int y = ((int) screenBounds().getHeight() / 2);
+
+        moveMouseToOffset(x, y);
+        performGesture(Gestures.doubleClickMouse());
+    }
+
+    /**
+     * not yet working if selecting involves scrolling the text field
+     */
+    public void selectWithMouse(TextOccurence textOccurence) {
+        TextSearchMatcher search = new TextSearchMatcher(textOccurence);
+        is(search);
+        final int textStartIndex = search.getStart();
+        final int textEndIndex = search.getEnd();
+
+        int scrollOffset = scrollPositionIntoView(textStartIndex + 1);
+
+        final int x = getTextWidth(0, textStartIndex) + getInsets().left - scrollOffset;
+        final int y = ((int) screenBounds().getHeight() / 2);
+
+        moveMouseToOffset(x, y);
+
+        performGesture(whileHoldingMouseButton(Gestures.BUTTON1,
+                translateMouseTo(new Tracker() {
+                    public Point target(Point currentLocation) {
+                        SelectionManipulation selectionManipulation = new SelectionManipulation();
+                        perform("selection", selectionManipulation);
+
+                        int direction = selectionManipulation.selectionEnd > textEndIndex ? -1 : 1;
+
+                        if(!screenBounds().contains(currentLocation)) {///oops, we're off the edge... this should get handled below but for some reason it doesn't quite work
+                            return translateHorizontally(currentLocation, direction, 10);
+                        }
+
+                        int numberOfCharactersToMove = selectionManipulation.selectionEnd - textEndIndex;
+
+                        //if we're more than 2 characters away move quickly (5 at a time), else move slowly
+                        int amount = Math.abs(numberOfCharactersToMove) > 2 ? 5 : 1;
+
+                        if (numberOfCharactersToMove != 0) {
+                            return translateHorizontally(currentLocation, direction, amount);
+                        }
+                        return currentLocation;
+                    }
+
+                    private Point translateHorizontally(Point currentLocation, int direction, int amount) {
+                        Point desiredLocation = new Point(currentLocation);
+                        desiredLocation.translate(direction * amount, 0);
+                        return desiredLocation;
+                    }
+
+                })
+        ));
+    }
+
+
+    public Insets getInsets() {
+        ComponentInsetsManipulation componentInsetsManipulation = new ComponentInsetsManipulation();
+        perform("component insets", componentInsetsManipulation);
+        return componentInsetsManipulation.getInsets();
+    }
+
+    @Override
+    public void moveCaretTo(final int position) {
+        perform("moving caret", new ComponentManipulation<JTextField>() {
+            public void manipulate(JTextField component) {
+                component.setCaretPosition(position);
+            }
+        });
+    }
+
+    private int getTextWidth(int startIndex, int endIndex) {
+        TextWidthManipulation widthManipulation = new TextWidthManipulation(startIndex, endIndex);
+        perform("Text width", widthManipulation);
+        return widthManipulation.getWidth();
+    }
+
+    /**
+     * bodge to scroll the text field so the word is in view - would normally do this with the keyboard or mouse
+     */
+    private int scrollPositionIntoView(int start) {
+        moveCaretTo(0);
+        moveCaretTo(start);
+
+        OffsetManipulation offsetManipulation = new OffsetManipulation();
+        perform("get scroll offset", offsetManipulation);
+        return offsetManipulation.getOffset();
+    }
+
+    public void focusWithMouse() {
+        performGesture(moveMouseTo(centerOfComponent()), clickMouseButton(BUTTON1));
+    }
+
+    public void caretPositionIs(final int caretPosition) {
+        is(new TypeSafeMatcher<JTextField>() {
+            public boolean matchesSafely(JTextField item) {
+                return item.getCaretPosition() == caretPosition;
+            }
+
+            public void describeTo(Description description) {
+                description.appendText("caret position");
+            }
+        });
+    }
+
+    private static class ComponentInsetsManipulation implements ComponentManipulation<JTextField> {
+        private Insets insets;
+
+        public void manipulate(JTextField component) {
+            this.insets = component.getInsets();
+        }
+
+        public Insets getInsets() {
+            return insets;
+        }
+    }
+
+    private static class OffsetManipulation implements ComponentManipulation<JTextField> {
+        public int offset;
+
+        public void manipulate(JTextField component) {
+            offset = component.getScrollOffset();
+
+        }
+
+        public int getOffset() {
+            return offset;
+        }
+    }
+
+    private static class SelectionManipulation implements ComponentManipulation<JTextField> {
+        int selectionStart;
+        int selectionEnd;
+
+        public void manipulate(JTextField component) {
+            selectionStart = component.getSelectionStart();
+            selectionEnd = component.getSelectionEnd();
+        }
+    }
+}
