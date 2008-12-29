4f2a4e3c2dafcdadb6c514acd6f0e9a381170386
smgfreeman <smgfreeman@ac37430e-0f47-0410-9073-f3f9481b7c33>
2008-12-29 15:36:51 +0000

    Fixed comparisons string in failure report test

--- src/swing/tests/com/objogate/wl/swing/driver/tests/JTableDriverTest.java
+++ src/swing/tests/com/objogate/wl/swing/driver/tests/JTableDriverTest.java
@@ -75,7 +75,7 @@ public class JTableDriverTest extends AbstractComponentDriverTest<JTableDriver> 
         smallDriver.hasRow(IterableComponentsMatcher.matching(withLabelText("2x1"), withLabelText("1x2"), withLabelText("2x3")));
       } catch (AssertionError expected) {
         assertThat(expected.getMessage(), 
-                   containsString("row with cells label with text \"2x1\", label with text \"1x2\", label with text \"2x3\""));
+                   containsString("row with cells <label with text \"2x1\">, <label with text \"1x2\">, <label with text \"2x3\">"));
         return;
       }
       fail("Should have failed");
